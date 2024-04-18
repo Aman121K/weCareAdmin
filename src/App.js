4589ac2e0328163a582459eb0abd18fa0abb744c
abc <abc@abcs-MacBook-Pro.local>
2024-04-18 15:48:51 +0530

    logout feature added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from "react-router-dom";
 import Dashboard from './Dashboard';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
-import { Routes, Route } from "react-router-dom"
 import SubAdmin from './Subadmin';
 import Reports from './Reports';
 import AddUser from './Adduser';
+import LoginPage from './login';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import LoginPage from './login';
+// import { Navbar } from 'react-bootstrap';
+
 function App() {
+  const [userIsLogging, setUserIsLogging] = useState(true);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = () => {
+    const loginData = localStorage.getItem('userData');
+    if (loginData) {
+      setUserIsLogging(true);
+    }else{
+      setUserIsLogging(true)
+    }
+  };
+
   return (
     <div>
-      <Navbar />
-      <div class="container-fluid" id="main">
-        <div class="row row-offcanvas row-offcanvas-left">
-          <Sidebar/>
-            <Routes>
-            <Route path="/" element={<LoginPage />} />
-              <Route path="/dash" element={<Dashboard />} />
-              <Route path="/subadmin" element={<SubAdmin />} />
-              <Route path="/reports" element={<Reports />} />
-              <Route path="/adduser" element={<AddUser />} />
-            </Routes>
-
-          {/* </Sidebar> */}
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        {/* {!userIsLogging && ( */}
+          <Route
+            path="*"
+            element={
+              <>
+                <Navbar />
+                <div className="container-fluid" id="main">
+                  <div className="row row-offcanvas row-offcanvas-left">
+                    <Sidebar />
+                    <Routes>
+                      <Route path="/dash" element={<Dashboard />} />
+                      <Route path="/subadmin" element={<SubAdmin />} />
+                      <Route path="/reports" element={<Reports />} />
+                      <Route path="/adduser" element={<AddUser />} />
+                    </Routes>
+                    {/* </Sidebar> */}
+                  </div>
+                </div>
+              </>
+            }
+          />
+        {/* )} */}
+      </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
         <div className="col-md-3 col-lg-2 sidebar-offcanvas pl-0" id="sidebar" role="navigation" >
             <ul className="nav flex-column sticky-top pl-0 pt-5 p-3 mt-3 ">
                 <li className="nav-item mb-2 mt-3"><a className="nav-link text-secondary" href="#"><h5>We care</h5></a></li>
-                <Link to="/">
+                <Link to="/dash">
                     <li className="nav-item mb-2 "><a className="nav-link text-secondary" href="#"><i className="fas fa-home font-weight-bold"></i> <span className="ml-3">Overview</span></a></li>
                 </Link>
                 <Link to="/subadmin">
@@ -21,6 +21,9 @@ const Sidebar = () => {
                 </Link>
                 <Link to="/reports">
                     <li className="nav-item mb-2"><a className="nav-link text-secondary" href="#"><i className="far fa-chart-bar font-weight-bold"></i> <span className="ml-3">Analytics</span></a></li>
+                </Link>
+                <Link to="/">
+                    <li className="nav-item mb-2"><a className="nav-link text-secondary" href="#"><i className="far fa-chart-bar font-weight-bold"></i> <span className="ml-3">Signout</span></a></li>
                 </Link>
                 {/* <li className="nav-item mb-2"><a className="nav-link text-secondary" href="#"><i className="fas fa-file-export font-weight-bold"></i><span className="ml-3">Export</span></a></li>
                 <li className="nav-item mb-2"><a className="nav-link text-secondary" href="#"><i className="fas fa-tablet-alt font-weight-bold"></i><span className="ml-3">Snippets</span></a></li>
